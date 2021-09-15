97b9a7b5cfb9784c7d7663edd06b17ad49a9e000
hiwad rashad <hiwadrashad112@hotmail.com>
2021-09-15 17:27:59 +0200

    Add project files.

---
+++ Audio-Synth/Audio/SampleGenerator.fsx
@@ -0,0 +1,17 @@
+﻿module SampleGenerator
+
+let generateSamples milliseconds frequency = 
+ let sampleRate = 44100.
+ let sixteenBitsampleLimit = 32767.
+ let VOLUME = 0.8
+ let toAmplitude x = 
+   x 
+   |> (*) (2. * System.Math.PI * frequency / sampleRate) 
+   |> sin
+   |> (*) sixteenBitsampleLimit
+   |> (*) VOLUME
+   |> int16
+ let numOfSamples = milliseconds / 1000. * sampleRate
+ let requiredSamples = seq {1.0..numOfSamples}
+ let result = Seq.map toAmplitude requiredSamples 
+ result
---
+++ Audio-Synth/Parsers/ComposerParsing.fsx
@@ -0,0 +1,36 @@
+﻿#r @"C:\Users\itv-admin\.nuget\packages\fparsec\1.1.1\lib\netstandard2.0\FParsec.dll"
+#r @"C:\Users\itv-admin\.nuget\packages\fparsec\1.1.1\lib\netstandard2.0\FParsecCS.dll"
+
+open FParsec
+
+let TST p str = 
+ match run p str with
+ |Success(result, _,  _) -> printfn "Success: %A" result
+ | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+type MeasureFraction = Half | Quarter | Eighth | Sixteenth | ThirthySeconth
+type Length = {fraction: MeasureFraction; extended: bool}
+type Note = A | ASharp | B | C | CSharp | D | DSharp | E | F | FSharp | G | GSharp
+type Octave = One | Two | Three
+type Sound = Rest | Tone of note: Note * octave: Octave
+type Token = {length: Length; sound: Sound}
+
+let aspriation = "32.#d3"
+
+let parsemeasurefraction = 
+ (stringReturn "2" Half) <|> (stringReturn "4" Quarter) <|> (stringReturn "8" Eighth) <|> (stringReturn "16" Sixteenth) <|> (stringReturn "32" ThirthySeconth)
+
+let parseextendedparser = (stringReturn "." true) <|> (stringReturn "" false)
+
+let parselength = 
+ pipe2
+  parsemeasurefraction
+  parseextendedparser
+  (fun t e -> {fraction = t; extended = e})
+
+let parsenotsharpablenote = anyOf "be" |>>(function
+ | 'b' -> B
+ | 'e' -> E)
+ | unknown -> sprintf "Unknown note %c" unknown |> failwith
+
+TST parselength aspriation
